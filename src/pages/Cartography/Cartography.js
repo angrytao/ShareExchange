d1807e7f35b4490168f662442c9af39544169dc8
bingotao <444034939@qq.com>
2018-11-21 10:57:34 +0800

    项目初始化

---
+++ src/pages/Cartography/Cartography.js
@@ -0,0 +1,13 @@
+import {Component} from 'react';
+import st from './Cartography.less';
+
+class Cartography extends Component{
+
+    render(){
+        return (<div className={st.Cartography}>
+            Cartography
+        </div>);
+    }
+}
+
+export default Cartography;
---
+++ src/pages/DataExchange/DataExchange.js
@@ -0,0 +1,13 @@
+import {Component} from 'react';
+import st from './DataExchange.less';
+
+class DataExchange extends Component{
+
+    render(){
+        return (<div className={st.DataExchange}>
+            DataExchange
+        </div>);
+    }
+}
+
+export default DataExchange;
