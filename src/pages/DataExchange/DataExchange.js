1f0ad0f3d6906b6c9ea593011bd9e9b2ea0d143f
bingotao <444034939@qq.com>
2018-11-21 10:58:48 +0800

    项目初始化

--- src/pages/DataExchange/DataExchange.js
+++ src/pages/DataExchange/DataExchange.js
@@ -1,13 +1,10 @@
-import {Component} from 'react';
+import { Component } from 'react';
 import st from './DataExchange.less';
 
-class DataExchange extends Component{
-
-    render(){
-        return (<div className={st.DataExchange}>
-            DataExchange
-        </div>);
-    }
+class DataExchange extends Component {
+  render() {
+    return <div className={st.DataExchange}>DataExchange</div>;
+  }
 }
 
 export default DataExchange;
